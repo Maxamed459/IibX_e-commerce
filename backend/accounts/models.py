5b6fb0abe25b812017f696dc09f32a012d1e9986
Maxamed459 <maxamedmahdi459@gmail.com>
2026-04-21 12:20:45 +0300

    refactor(backend): renamed a field from User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(max_length=20, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    date_changed = models.DateTimeField(default=timezone.now)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     objects = UserManager()
 
