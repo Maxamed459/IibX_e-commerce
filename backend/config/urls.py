ab55b3d96170b0f2cc15c0255dbef1a55331cfea
Maxamed459 <maxamedmahdi459@gmail.com>
2026-05-18 09:01:36 +0300

    fix(backend): fixed the swagger ui endpoints for api docs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,13 +18,17 @@ urlpatterns = [
     # API v1
     path("api/v1/", include(api_v1_patterns)),
     # API Documentation
-    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/docs/",
+        "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger_ui",
+        name="swagger-ui",
     ),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 if settings.DEBUG:
